8d51fe0bfeb9884157e0ff76a989ab9e81d20dd9
Michael Bruns <michaelbruns99@gmail.com>
2017-05-13 01:07:05 -0400

    created feed folder and sub folders and setup files

---
+++ app/feed/following/route.js
@@ -0,0 +1,8 @@
+import AbstractIndexRoute from 'hospitalrun/routes/abstract-index-route';
+import Ember from 'ember';
+import { translationMacro as t } from 'ember-i18n';
+import UserSession from 'hospitalrun/mixins/user-session';
+
+const { computed } = Ember;
+
+export default AbstractIndexRoute.extend();
