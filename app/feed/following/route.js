d73dc6a22380bc1aa26312f8e2447fdeaceb97b0
Michael Bruns <michaelbruns99@gmail.com>
2017-06-28 21:42:39 -0400

    added routes and templates

--- app/feed/following/route.js
+++ app/feed/following/route.js
@@ -1,3 +1,4 @@
+//Modeled off of /app/labs/index/
 import AbstractIndexRoute from 'hospitalrun/routes/abstract-index-route';
 import Ember from 'ember';
 import { translationMacro as t } from 'ember-i18n';
@@ -5,4 +6,28 @@ import UserSession from 'hospitalrun/mixins/user-session';
 
 const { computed } = Ember;
 
-export default AbstractIndexRoute.extend();
+export default AbstractIndexRoute.extend(UserSession, {
+  modelName: 'following',
+  // newButtonAction: computed(function() {
+  //   if (this.currentUserCan('add_patient')) {
+  //     return 'newItem';
+  //   } else {
+  //     return null;
+  //   }
+  // }),
+  // newButtonText: t('patients.buttons.newPatient'),
+  // pageTitle: t('patients.titles.patientListing'),
+
+  _getStartKeyFromItem(item) {
+    let displayPatientId = item.get('displayPatientId');
+    let id = this._getPouchIdFromItem(item);
+    return [displayPatientId, id];
+  },
+
+  _modelQueryParams() {
+    return {
+      mapReduce: 'patient_by_display_id'
+    };
+  }
+
+});
--- app/feed/index/route.js
+++ app/feed/index/route.js
@@ -5,4 +5,28 @@ import UserSession from 'hospitalrun/mixins/user-session';
 
 const { computed } = Ember;
 
-export default AbstractIndexRoute.extend();
+export default AbstractIndexRoute.extend(UserSession, {
+  modelName: 'feed',
+  newButtonAction: computed(function() {
+    if (this.currentUserCan('add_patient')) {
+      return 'newItem';
+    } else {
+      return null;
+    }
+  }),
+  newButtonText: t('patients.buttons.newPatient'),
+  pageTitle: t('patients.titles.patientListing'),
+
+  _getStartKeyFromItem(item) {
+    let displayPatientId = item.get('displayPatientId');
+    let id = this._getPouchIdFromItem(item);
+    return [displayPatientId, id];
+  },
+
+  _modelQueryParams() {
+    return {
+      mapReduce: 'patient_by_display_id'
+    };
+  }
+
+});
